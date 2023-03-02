4326b1c54dd7e1c7a9f5eea4c5e712160d76999b
unknown <abusiddiq200g3@gmail.com>
2023-03-02 10:08:42 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,158 @@
+// // how to print strings in javascript
+
+// console.log('hello world');
+
+// console.log('MOHAMMED ABUBAKAR SIDDIQ');
+
+
+
+// // printing a joined or concatenated sentence
+
+// console.log('hello '+ 'Mohammed Abubakar Siddiq ' + 'How are you doing ')
+
+// console.log('My Name Is ' + 'Mohammed Abubakar siddiq ' + 'i am learning web development')
+
+// // Acessing string charaters=>log('hi there' [index])
+
+// console.log('SIDDIQ'[0])
+
+// // Acessing string lenght 
+
+// console.log('siddiq'.length)
+
+// ///////////////////=VARIABLE=/////////////////////////
+
+// //////declaring variable///////////////
+
+// let num =500
+// console.log(num)
+
+// let person = ' Mohammmed Abubakar Siddiq' 
+// console.log(person)
+
+// /// declaring two variables at a time ///
+
+// let firstName = 'Abubakar Siddiq'
+// let lastName = 'Mohammed'
+
+// console.log(firstName,lastName)
+
+// /// constant variables///
+
+// const Name = 'Abubakar Siddiq'
+// console.log(Name)
+
+// // Exercise on strings  and variable// 
+
+// let celsius = 30
+
+// // formula to convert celsius to fahrenheit => F = C * 1.8 + 32
+
+// let fahrenheit = celsius* 1.8+32
+// console.log (fahrenheit) 
+
+// ////////// Equality Operators/////////
+
+// let bool ='1'== 1
+// console.log(bool);
+
+// //////////////// hen to fowl/////////////////
+
+// let poultry = 'hen' == 'fowl'
+// console.log(poultry);
+
+// /////////// Conditionals ///////////////////////
+
+// let shopopen = true
+// let cost100 = true
+
+// /////////////////////////
+
+// if( shopopen && cost100 === true){
+//     console.log('product bought successfully');
+// }
+
+// ////////////////////
+
+// else if (shopopen && cost100 ===false){
+//     console.log('Sorry we are closed');
+// }
+
+// ////////////// else if ///////////// 
+
+// let hot = true
+
+// if(hot === true){
+//     console.log('weather is too hot')
+//     }
+//     else{
+//         console.log ('weather is cold')
+//     }
+
+//     ////////// TERNARY OPERATORS//////////
+    
+//    let temperature= hot ? 'weather is too hot' : 'weather is cold' 
+//    console.log(temperature)
+
+//    ///////////////////// loop//////////////////
+//    ///////////// for loop //////////////////
+
+//    for(let i = 0; i <= 50; i++) {
+//     console.log(i);
+//    }
+
+   /////////////////// function /////////////////////////
+
+//    function siddiq (x,y){
+//     console.log (x+y)
+//    }
+//    siddiq(10,7)
+
+// function siddiq (a,b){
+//     console.log(a+b)
+// }
+// siddiq(2,7)
+
+// function sid (firstName,lastName){
+//     console.log(firstName+lastName)
+// }
+
+// sid ('Abubakar', 'Mohammed')
+
+/////////// for loop /////////////////
+
+for(let i = 0; i <= 20; i++){
+    console.log(i)
+}
+
+////////////// Q2 ////////////
+
+for (let i=0 ;i <= 20; i++){
+    if(i% 3===0){
+        console.log('code')
+    }
+   
+
+}
+
+//////////// Q3 ///////////////
+
+for(let i= 0; i <= 20 ; i++){
+    if(i% 5===0){
+        console.log('coast')
+    }
+   
+}
+
+/////////// Q4 ///////////////
+
+for(let i= 0; i <= 20 ; i++){
+    if(i% 3===0 && i% 5===0) {
+        console.log('code coast')
+    }
+   
+}
+
+////////// Q5 ///////////////
+
+
